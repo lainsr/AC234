b40db06ec070409a1f5f77a037d3c8eebefa473c
Stéphane Rossé <s.rosse@cyberiacafe.ch>
2012-09-05 22:20:56 +0200

    Remove old and unused stuff, merge the 2 datamodels in one

---
+++ AC234/Classes/UI/Settings/ACSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACSettingsViewController.h
+//  AC234
+//
+//  Created by Stéphane Rossé on 05.09.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACSettingsViewController : UIViewController
+
+@end
---
+++ AC234/Classes/UI/Settings/ACSettingsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ACSettingsViewController.m
+//  AC234
+//
+//  Created by Stéphane Rossé on 05.09.12.
+//
+//
+
+#import "ACSettingsViewController.h"
+
+@interface ACSettingsViewController ()
+
+@end
+
+@implementation ACSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
